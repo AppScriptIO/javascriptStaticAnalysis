3772fa3d70373ff2d4c9eb736e88077cee856d7e
Safi <sfmissive@gmail.com>
2019-03-14 10:22:36 +0200

    ✒ Load typescript & Babel config. Make path to config customizable

--- source/entrypoint.js
+++ source/entrypoint.js
@@ -1,10 +1,13 @@
 const   path = require('path'),
         { addModuleResolutionPathMultiple } = require(`@dependency/addModuleResolutionPath`)
-
-const nodeModulesPath = path.dirname(path.dirname(path.dirname( require.resolve('eslint/package.json') ))) // get the node_modules folder where plugins are installed. Could be own package root or parent packages root (when this modules is installed as a pacakge)
-addModuleResolutionPathMultiple({ pathArray: [ nodeModulesPath ] }) // Add own node_modules to module resolving paths
+let eslintJSLinterFunc = require('./eslint.config.js')
+let prettierJSFormatter = require('./prettier.config.js')
 
 module.exports = {
-  eslintJSLinter: require('./eslint.config.js'),
-  prettierJSFormatter: require('./prettier.config.js')
-}
+  initialize: () => {
+    const nodeModulesPath = path.dirname(path.dirname(path.dirname( require.resolve('eslint/package.json') ))) // get the node_modules folder where plugins are installed. Could be own package root or parent packages root (when this modules is installed as a pacakge)
+    addModuleResolutionPathMultiple({ pathArray: [ nodeModulesPath ] }) // Add own node_modules to module resolving paths
+  }, 
+  eslintJSLinterFunc,
+  prettierJSFormatter
+} 
--- source/eslint.config.js
+++ source/eslint.config.js
@@ -2,58 +2,64 @@ let typescriptEslintRecommended = require('@typescript-eslint/eslint-plugin/dist
 let prettierTypescriptEslint = require('eslint-config-prettier/@typescript-eslint')
 let prettierConfig = require('./prettier.config.js')
 
-module.exports = {
-  root: true,
-  overrides: [
-    // { // NOTE: Using VSCode builtin formatter instead
-    //   files: ['**.json', '**.jsonc'],
-    //   plugins: ['eslint-plugin-json'],
-    // },
-    // specific different parser according to file extension.
-    {
-      files: ['**.js'],
-      // "excludedFiles": "*.test.js",
-      parser: 'babel-eslint',
-      parserOptions: {
-        babelOptions: {
-          configFile: './babel.config.js',
-        },
-      },
-      plugins: [
-        'eslint-plugin-babel', // eslint-plugin-babel re-implements (from the base eslint rules) problematic rules so they do not give false positives or negatives
-        'prettier',
-      ],
-      rules: {
-        'prettier/prettier': [
-          'warn',
-          prettierConfig,
-          {
-            usePrettierrc: true,
+module.exports = ({
+  babelConfigPath = './babel.config.js',
+  typescriptConfigPath = './typescript.config.json'
+} = {}) => {
+  return {
+    root: true,
+    overrides: [
+      // { // NOTE: Using VSCode builtin formatter instead
+      //   files: ['**.json', '**.jsonc'],
+      //   plugins: ['eslint-plugin-json'],
+      // },
+      // specific different parser according to file extension.
+      {
+        files: ['**.js'],
+        // "excludedFiles": "*.test.js",
+        parser: 'babel-eslint',
+        parserOptions: {
+          babelOptions: {
+            configFile: babelConfigPath,
           },
-        ],
-      },
-    },
-    {
-      files: ['**.ts'],
-      // correctly parse typescript with babel parser - https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/typescript-estree/tests/ast-alignment/parse.ts#L16
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module',
-      },
-      plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
-      rules: Object.assign(
-        typescriptEslintRecommended.rules,
-        prettierTypescriptEslint.rules, // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-        // prettier Eslint Recommended are `prettier` plugin and `error` rule // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-        {
-          'prettier/prettier': 'error',
         },
-      ),
+        plugins: [
+          'eslint-plugin-babel', // eslint-plugin-babel re-implements (from the base eslint rules) problematic rules so they do not give false positives or negatives
+          'prettier',
+        ],
+        rules: {
+          'prettier/prettier': [
+            'warn',
+            prettierConfig,
+            {
+              usePrettierrc: true,
+            },
+          ],
+        },
+      },
+      {
+        files: ['**.ts'],
+        // correctly parse typescript with babel parser - https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/typescript-estree/tests/ast-alignment/parse.ts#L16
+        parser: '@typescript-eslint/parser',
+        parserOptions: {
+          ecmaVersion: 2018,
+          sourceType: 'module',
+          project: typescriptConfigPath // should be consumed by a function and replaced.
+        },
+        plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
+        rules: Object.assign(
+          typescriptEslintRecommended.rules,
+          prettierTypescriptEslint.rules, // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+          // prettier Eslint Recommended are `prettier` plugin and `error` rule // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+          {
+            'prettier/prettier': 'error',
+          },
+        ),
+      },
+    ],
+    rules: {},
+    env: {
+      node: true,
     },
-  ],
-  rules: {},
-  env: {
-    node: true,
-  },
+  }
 }
