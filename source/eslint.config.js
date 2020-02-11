2ad67a3cfe4b5b85c0ed5a475f7ee3b54fb82aa5
Safi <nassarsafi@gmail.com>
2020-02-12 00:57:23 +0200

    ✒ Add browser js eslint config. And clear up code.

--- source/eslint.config.js
+++ source/eslint.config.js
@@ -1,65 +1,128 @@
-let typescriptEslintRecommended = require('@typescript-eslint/eslint-plugin/dist/configs/recommended.json')
-let prettierTypescriptEslint = require('eslint-config-prettier/@typescript-eslint')
-let prettierConfig = require('./prettier.config.js')
+module.exports.nodejsEnvironment = ({ babelConfigPath = './configuration/babel.config.js', typescriptConfigPath = './configuration/typescript.config.json' } = {}) => {
+  let typescriptEslintRecommended = require('@typescript-eslint/eslint-plugin/dist/configs/recommended.json')
+  let prettierTypescriptEslint = require('eslint-config-prettier/@typescript-eslint')
+  let prettierConfig = require('./prettier.config.js')
 
-module.exports = ({
-  babelConfigPath = './configuration/babel.config.js',
-  typescriptConfigPath = './configuration/typescript.config.json'
-} = {}) => {
-  return {
+  let vscodeBuildInFormater = {
+    // NOTE: Using VSCode builtin formatter instead
+    files: ['**.json', '**.jsonc'],
+    plugins: ['eslint-plugin-json'], // build-in plugins.
+  }
+
+  let nodejsFile = {
+    files: ['**.js'],
+    // "excludedFiles": "*.test.js",
+    parser: 'babel-eslint',
+    parserOptions: {
+      babelOptions: {
+        configFile: babelConfigPath,
+      },
+    },
+    plugins: [
+      'eslint-plugin-babel', // eslint-plugin-babel re-implements (from the base eslint rules) problematic rules so they do not give false positives or negatives
+      'prettier',
+    ],
+    rules: {
+      'prettier/prettier': [
+        'warn',
+        prettierConfig,
+        {
+          usePrettierrc: true,
+        },
+      ],
+    },
+  }
+
+  let typescriptFile = {
+    files: ['**.ts'],
+    // correctly parse typescript with babel parser - https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/typescript-estree/tests/ast-alignment/parse.ts#L16
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+      project: typescriptConfigPath, // Follows the path should be consumed by a function and replaced.
+    },
+    plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
+    rules: Object.assign(
+      typescriptEslintRecommended.rules,
+      prettierTypescriptEslint.rules, // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+      // prettier Eslint Recommended are `prettier` plugin and `error` rule // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+      {
+        'prettier/prettier': 'warn',
+      },
+    ),
+  }
+
+  // eslint combined config with prettier integration.
+  let eslintConfig = {
     root: true,
     overrides: [
-      // { // NOTE: Using VSCode builtin formatter instead
-      //   files: ['**.json', '**.jsonc'],
-      //   plugins: ['eslint-plugin-json'],
-      // },
       // specific different parser according to file extension.
-      {
-        files: ['**.js'],
-        // "excludedFiles": "*.test.js",
-        parser: 'babel-eslint',
-        parserOptions: {
-          babelOptions: {
-            configFile: babelConfigPath,
-          },
-        },
-        plugins: [
-          'eslint-plugin-babel', // eslint-plugin-babel re-implements (from the base eslint rules) problematic rules so they do not give false positives or negatives
-          'prettier',
-        ],
-        rules: {
-          'prettier/prettier': [
-            'warn',
-            prettierConfig,
-            {
-              usePrettierrc: true,
-            },
-          ],
-        },
-      },
-      {
-        files: ['**.ts'],
-        // correctly parse typescript with babel parser - https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/typescript-estree/tests/ast-alignment/parse.ts#L16
-        parser: '@typescript-eslint/parser',
-        parserOptions: {
-          ecmaVersion: 2018,
-          sourceType: 'module',
-          project: typescriptConfigPath // Follows the path should be consumed by a function and replaced.
-        },
-        plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
-        rules: Object.assign(
-          typescriptEslintRecommended.rules,
-          prettierTypescriptEslint.rules, // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-          // prettier Eslint Recommended are `prettier` plugin and `error` rule // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-          {
-            'prettier/prettier': 'warn',
-          },
-        ),
-      },
+      // vscodeBuildInFormater,
+      nodejsFile,
+      typescriptFile,
     ],
     rules: {},
     env: {
       node: true,
     },
   }
+
+  console.log(`• ESlint config used:`)
+  console.log(eslintConfig)
+
+  return eslintConfig
+}
+
+module.exports.browserEnvironment = () => {  
+  /*
+  *https://github.com/open-wc/open-wc/blob/master/packages/prettier-config/prettier.config.js
+    Apply formatting to JS files
+    Apply formatting to HTML inside of html tagged template literals used by lit-html
+    Apply formatting to CSS inside of css tagged template literals used by lit-element
+    Integration with ESLint to prevent potentially conflicting rules
+
+  */
+  let prettierConfig = require('@open-wc/eslint-config') // https://github.com/open-wc/open-wc/ > eslint-config
+
+  /*
+  *https://github.com/open-wc/open-wc/blob/master/packages/eslint-config/index.js
+    Apply linting to js and html files
+    Apply linting for best practices
+    Allow dynamic module imports
+    Allow imports in test/demos from devDependencies
+    Allow underscore dangle
+    Do not prefer default exports
+    Do not prefer no file extension
+
+  USAGE: 
+    • require directly and make sure internal plugin names are resolved correctly (./@open-wc/eslint-config/node_modules/<plugins...>) 
+  OR
+    • use eslint config: (which may resolve dependent plugins correctly) https://github.com/open-wc/open-wc/blob/master/packages/eslint-config/package.json
+      "@open-wc/eslint-config",
+      "eslint-config-prettier"
+
+  */
+  let openWebcomponentEslintConfig = require('@open-wc/eslint-config') // https://github.com/open-wc/open-wc/ > eslint-config
+
+  let eslintConfig = {
+    extends: [       
+      "@open-wc/eslint-config",
+      "eslint-config-prettier"
+    ], 
+    rules: {
+      'prettier/prettier': [
+        'warn',
+        prettierConfig,
+        {
+          usePrettierrc: true,
+        },
+      ],
+    },
+  } 
+
+  console.log(`• ESlint config used:`)
+  console.log(eslintConfig)
+
+  return eslintConfig
 }
--- source/script.js
+++ source/script.js
@@ -1,13 +1,13 @@
-const   path = require('path'),
-        { addModuleResolutionPathMultiple } = require(`@dependency/handleModuleSystem`)
-let eslintJSLinterFunc = require('./eslint.config.js')
-let prettierJSFormatter = require('./prettier.config.js')
+const path = require('path'),
+  { addModuleResolutionPathMultiple } = require(`@dependency/handleModuleSystem`), 
+  eslintJSLinterFunc = require('./eslint.config.js'), 
+  prettierJSFormatter = require('./prettier.config.js')
 
 module.exports = {
   initialize: () => {
-    const nodeModulesPath = path.dirname(path.dirname(path.dirname( require.resolve('eslint/package.json') ))) // get the node_modules folder where plugins are installed. Could be own package root or parent packages root (when this modules is installed as a pacakge)
-    addModuleResolutionPathMultiple({ pathArray: [ nodeModulesPath ] }) // Add own node_modules to module resolving paths
-  }, 
+    const nodeModulesPath = path.dirname(path.dirname(path.dirname(require.resolve('eslint/package.json')))) // get the node_modules folder where plugins are installed. Could be own package root or parent packages root (when this modules is installed as a pacakge)
+    addModuleResolutionPathMultiple({ pathArray: [nodeModulesPath] }) // Add own node_modules to module resolving paths
+  },
+  prettierJSFormatter, // prettier is being integrated from within eslint config.
   eslintJSLinterFunc,
-  prettierJSFormatter
-} 
+}
